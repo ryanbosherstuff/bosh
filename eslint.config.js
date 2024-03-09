9e1617b1e82274322ef10f01f32b99bb52527262
Ryan Bosher <ryan.bosher@stuff.co.nz>
2024-03-09 14:26:57 +1300

    init

---
+++ eslint.config.js
@@ -0,0 +1,92 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const nxEslintPlugin = require('@nx/eslint-plugin');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginSimpleImportSort = require('eslint-plugin-simple-import-sort');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  {
+    plugins: {
+      '@nx': nxEslintPlugin,
+      import: eslintPluginImport,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+  },
+  { languageOptions: { parserOptions: { sourceType: 'module' } } },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+          disallowTypeAnnotations: true,
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: ':matches(ExportAllDeclaration)',
+          message: 'Export only the modules you need.',
+        },
+      ],
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            [
+              '^(@stuff)(/.*|$)',
+              '^(@masthead)(/.*|$)',
+              '^(@uira)(/.*|$)',
+              '^(@common)(/.*|$)',
+              '^(@native)(/.*|$)',
+              '^(@plugins)(/.*|$)',
+            ],
+            ['^\\u0000'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  })),
+  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  })),
+  ...compat.config({ env: { jest: true } }).map((config) => ({
+    ...config,
+    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+    rules: {},
+  })),
+];
--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import { getJestProjects } from '@nx/jest';
 
+import { type Config } from 'jest';
+
 export default {
   projects: getJestProjects(),
-};
+} satisfies Config;
